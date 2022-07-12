4f656d1829326dd2efe8af40ef28a8b70cf35205
GiovanaAlonso <giovanaalonso@hotmail.com>
2022-07-12 20:44:08 -0300

    criando a classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,9 +1,16 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.outra.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
     }
+
+
+
+
 }
